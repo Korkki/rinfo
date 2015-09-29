96e64ddba8c8cf1f9dd9e0ec2a578c12a1199ff3
Rafal Korkosz <korkosz.rafal@gmail.com>
2015-09-29 23:09:33 +0200

    add redis conf

--- info/settings/prod.py
+++ info/settings/prod.py
@@ -1,8 +1,21 @@
+from urllib.parse import urlparse
 import dj_database_url
 from .base import *
 
 DATABASES = {
     'default': dj_database_url.config()
+}
+
+redis_url = urlparse(os.environ.get('REDIS_URL'))
+CACHES = {
+    "default": {
+        "BACKEND": "redis_cache.RedisCache",
+        "LOCATION": "{0}:{1}".format(redis_url.hostname, redis_url.port),
+        "OPTIONS": {
+            "PASSWORD": redis_url.password,
+            "DB": 0,
+        }
+    }
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
